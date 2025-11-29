4c9c29ab82a49198604c5bcb2e3e187306c0ec89
parveen-gitcode <dudekulaparveenatp@gmail.com>
2025-11-29 16:32:16 +0530

    Change CORS origin from localhost:3000 to localhost:5000

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "http://localhost:3000",
+      "http://localhost:5000",
       "https://aichatpage-parveen.vercel.app",   // your deployed frontend
     ],
     credentials: true,
